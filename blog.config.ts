8dd78f9438a9a2314e15866e0b6dfb12d715e871
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2026-07-02 14:03:44 +0800

    Update blog.config.ts

--- blog.config.ts
+++ blog.config.ts
@@ -77,8 +77,8 @@ export const MENU_ITEMS = [
 // 8. 侧边栏底部的社交/推荐链接：
 //预置了一个免费图床和一个高清图片壁纸站按钮,由于后者被墙，需要把后者的图片放到前面的图床后，再复制链接给你的博客配图
 export const RECOMMENDED_LINKS = [
-  { label: 'GitHub-Stars', url: 'http://github.orrz.de5.net', icon: Share2 },
-  { label: 'MoonTV', url: 'https://orrz.de5.net', icon: Tv },
+  { label: 'GitHub', url: 'http://github.orrz.de5.net', icon: Share2 },
+  { label: '观影计划', url: 'https://moon.orrz.de5.net/', icon: Tv },
   { label: '第三方图床', url: 'https://urusai.cc', icon: Image },
   { label: '壁纸图片网站', url: 'https://wallhaven.cc', icon: Image },
 ];
